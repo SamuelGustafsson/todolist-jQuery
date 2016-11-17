6dc150e005c8347319f3bf8ee3558818ecc572ce
SamuelGustafsson <samuelgustafsson890623@hotmail.com>
2016-11-17 14:13:16 +0100

    Functionallity build

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+   // When task is clicked make it done with line-through
+   $('ul').on('click', 'li', function () { 
+      $(this).toggleClass('task-done');
+   });
+
+   // Delete task
+   $('ul').on('click', 'span', function (event) {
+      $(this).parent().fadeOut(500, function(){
+         $(this).remove();
+      })     
+      event.stopPropagation();
+   });
+
+   // Add task
+   $('input').keypress(function (event) {
+      if (event.which === 13) {
+         var todoHtmlString = "<li><span>X</span> " + $('input').val() + "</li>"
+         $('ul').append(todoHtmlString);
+         $('input').val("");
+      }
+   });
+
+});
+
+
